b9dca8cdf6e3d4e64fc508d40d76f68dbde84994
Afsalms <afsalms93.mec@gmail.com>
2018-12-09 19:24:41 +0530

    Completed the code for the dice roller app

--- app/src/main/java/com/example/afsal/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/afsal/dicerollerapp/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.afsal.dicerollerapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+
+    var image : ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        image = findViewById(R.id.image_view)
+        image?.setOnClickListener(View.OnClickListener {
+            rollDice()
+        })
+
+    }
+
+    fun rollDice(){
+
+        var randInt = Random.nextInt(1,7)
+        when(randInt){
+            1 -> {image?.setImageResource(R.drawable.dice_one)}
+            2 -> {image?.setImageResource(R.drawable.dice_two)}
+            3 -> {image?.setImageResource(R.drawable.dice_three)}
+            4 -> {image?.setImageResource(R.drawable.dice_four)}
+            5 -> {image?.setImageResource(R.drawable.dice_five)}
+            6 -> {image?.setImageResource(R.drawable.dice_six)}
+
+        }
     }
 }
